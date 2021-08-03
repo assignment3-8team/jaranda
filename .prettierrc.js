f39a343c8d22db4ec4c8583cbd07fb9623bd2c70
sunhwa <2017sunhwa@gmail.com>
2021-08-03 14:07:14 +0900

    🎨[style]prettier 추가

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 150, // 화면 폭
+  tabWidth: 2, // 탭 간격, 스페이스 설정 시 2칸 띄어쓰기
+  trailingComma: "all",
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+};

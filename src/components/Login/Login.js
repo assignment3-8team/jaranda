c77f92acc98d6213626f9d5b849d6cd871557161
kimjunyoung <kimjy392@gmail.com>
2021-08-05 02:17:56 +0900

    :hammer:[fix] Form 유효성 검사 로직 custom hook으로 분리 및 Login에 쓰이는 상수 추가

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,59 +1,56 @@
-import { useState, useRef } from 'react'
-import { SingleInput } from './SingleInput'
+import { SingleInput } from "./SingleInput";
+import { useForm } from "./useForm";
+import { LOGIN_FORM } from "../../constants/loginForm";
+import "./style.css";
+
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+const validations = {
+  email: {
+    pattern: {
+      value: LOGIN_FORM.REG_EMAIL,
+      message: LOGIN_FORM.ERROR_MESSAGE_EMAIL_PATTERN,
+    },
+    required: {
+      value: true,
+      message: LOGIN_FORM.ERROR_MESSAGE_EMAIL_REQUIRED,
+    },
+  },
+  password: {
+    pattern: {
+      value: LOGIN_FORM.REG_PASSWORD,
+      message: LOGIN_FORM.ERROR_MESSAGE_PASSWORD_PATTERN,
+    },
+    required: {
+      value: true,
+      message: LOGIN_FORM.ERROR_MESSAGE_PASSWORD_REQUIRED,
+    },
+  },
+};
+
 const Login = () => {
-  const isValid = useRef({})
-  const [inputs, setInputs] = useState({
-    email : '',
-    password : ''
-  })
-
-  const onChange = (e) => {
-    const name = e.target.name
-    setInputs({
-      ...inputs,
-      [name] : e.target.value
-    })
-  }
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-    let isAllValid = true;
-    for (let value of Object.values(isValid.current)) {
-      isAllValid = isAllValid && value.validity.valid
-    }
-    if (isAllValid) {
-      setInputs({
-        email: '',
-        password: ''
-      })
-    }
-  }
+  const onSubmit = e => {
+    // 로그인 로직
+    alert("로그인!");
+  };
+  const { data, onChange, handleSubmit, errors } = useForm({
+    initialValues,
+    validations,
+    onSubmit,
+  });
   return (
-    <form onSubmit={onSubmit} noValidate>
-      <SingleInput 
-        name="email" 
-        onChange={onChange}
-        value={inputs.email} 
-        placeholder="Email" 
-        type="email"
-        ref={e => {isValid.current["email"] = e}}
-        erroremessage="올바른 이메일을 입력해주세요" 
-        required
-      />
-      <SingleInput 
-        ref={e => {isValid.current["password"] = e}}
-        name="password" 
-        onChange={onChange} 
-        value={inputs.password} 
-        placeholder="password" 
-        type="password" 
-        erroremessage="특수문자와 숫자를 조합하여 16자 이내로 만들어주세요"
-        pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$" 
-        required
-      />
+    <form onSubmit={handleSubmit} noValidate>
+      <h2>로그인</h2>
+      <SingleInput name="email" onChange={onChange} value={data.email} placeholder="Email" />
+      {errors.email && errors.email.map((msg, idx) => <p key={idx}>{msg}</p>)}
+      <SingleInput name="password" onChange={onChange} value={data.password} placeholder="Password" type="password" />
+      {errors.password && errors.password.map((msg, idx) => <p key={idx}>{msg}</p>)}
       <button type="submit">로그인</button>
     </form>
-  )
-}
+  );
+};
 
-export { Login }
+export { Login };
--- src/components/Login/SingleInput.js
+++ src/components/Login/SingleInput.js
@@ -1,18 +1,3 @@
-import { useRef, forwardRef } from 'react'
-import './SingleInput.css'
-
-export const SingleInput = forwardRef((props, ref) => {
-  const isValid = useRef(null)
-
-  const onChange = (e) => {
-    const valid = e.target.validity.valid
-    isValid.current = valid
-    props.onChange(e)
-  }
-  return (
-    <>
-      <input {...props} onChange={onChange} ref={ref} />
-      {isValid.current === null || isValid.current ? null : <p>{props.erroremessage}</p>}
-    </>
-  )
-})
+export const SingleInput = props => {
+  return <input {...props} onChange={props.onChange} />;
+};

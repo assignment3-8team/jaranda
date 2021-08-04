ba4a3ba4d69ac548d948b6b6e4013e4240affbdc
yeoncho <yeoncho@student.42seoul.kr>
2021-08-04 09:14:29 +0900

    ✨feat: mockup 데이터를 활용한 pagination 구현

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,33 +1,30 @@
 import './style.css'
-import userData from 'mockup/userData.js';
 import TableItem from './TableItem';
-import { useState } from 'react';
 
-const Table = () => {
-    const [pageIndex, setPageIndex] = useState(1);
-    const maxIndex = Math.ceil(userData.length / 6);
-    const dataStartIndex = (pageIndex - 1) * 6; 
-    const renderData = userData.slice(dataStartIndex, dataStartIndex + 6);
-    console.log(renderData);
+
+const Table = (props) => {
+    const { renderData } = props;
 
     return (
-        <table className='user-info-table'>
-            <tbody>
-                <tr>
-                    <th>email</th>
-                    <th>name</th>
-                    <th>created date</th>
-                </tr>
-                {renderData.map((e, i) => (
-                    <TableItem
-                    key={e.user_id} 
-                    userEmail={e.user_email}
-                    userName={e.user_name}
-                    createdDate={e.created_date}
-                    />
-                ))}
-            </tbody>
-        </table>
+        <section className='user-info-table-wrap'>
+            <table className='user-info-table'>
+                <tbody>
+                    <tr>
+                        <th>email</th>
+                        <th>name</th>
+                        <th>created date</th>
+                    </tr>
+                    {renderData.map((e, i) => (
+                        <TableItem
+                        key={e.user_id} 
+                        userEmail={e.user_email}
+                        userName={e.user_name}
+                        createdDate={e.created_date}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        </section>
     )
 }
 
--- src/pages/Admin/TablePage.js
+++ src/pages/Admin/TablePage.js
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import './style.css'
 import Table from 'components/Table';
+import Pagination from 'components/Pagination';
+import userData from 'mockup/userData';
 
 const TablePage = (props) => {
+
+    const [pageIndex, setPageIndex] = useState(1);
+    const maxIndex = Math.ceil(userData.length / 6);
+    const dataStartIndex = (pageIndex - 1) * 6; 
+    const renderData = userData.slice(dataStartIndex, dataStartIndex + 6);
+
     return (
         <section>
             <header className="admin-header">
                 <span>관리자 페이지</span>
                 <span>admin menu</span>
             </header>
-            <Table/>
+            <Table renderData={renderData} />
+            <Pagination maxIndex={maxIndex} pageIndex={pageIndex} setPageIndex={setPageIndex}/>
         </section>
     )
 }

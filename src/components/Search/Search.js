ecb1364917151d47aafdfd7df119875d0d273ccd
MinhyukPark <parkminem@gmail.com>
2021-08-05 21:52:53 +0900

    :sparkles: feat: Search 기능 구현

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,28 +1,32 @@
 import "./style.css";
 import { useRef } from "react";
 
-const Search = ({}) => {
-  // const USER_INFO = [
-  //   {
-  //     user_email: "1@naver.com",
-  //     user_name: "박민혁",
-  //   },
-  //   {
-  //     user_email: "2@naver.com",
-  //     user_name: "김민혁",
-  //   },
-  //   {
-  //     user_email: "3@naver.com",
-  //     user_name: "이민혁",
-  //   },
-  // ];
+const Search = () => {
+  const userInfo = [
+    {
+      user_email: "1@naver.com",
+      user_name: "박민혁",
+    },
+    {
+      user_email: "2@naver.com",
+      user_name: "김민혁",
+    },
+    {
+      user_email: "3@naver.com",
+      user_name: "이민혁",
+    },
+    {
+      user_email: "3@naver.com",
+      user_name: "김민혁",
+    },
+  ];
 
   const inputRef = useRef();
 
   const handleSearch = () => {
     const value = inputRef.current.value;
-    console.log(value);
-    // onSearch(value);
+    const filteredData = userInfo.filter(e => e.user_name === value);
+    console.log(filteredData);
   };
 
   const onClick = () => {
@@ -37,13 +41,7 @@ const Search = ({}) => {
 
   return (
     <>
-      <input
-        className="search-input"
-        ref={inputRef}
-        type="serch"
-        placeholder="Search..."
-        onKeyPress={onKeyPress}
-      />
+      <input className="search-input" ref={inputRef} type="serch" placeholder="Search..." onKeyPress={onKeyPress} />
       <button className="search-button" type="submit" onClick={onClick}>
         검색
       </button>

dca993bf1d989a001fad9ed3e91bf6a9303b2a4f
sunhwa <2017sunhwa@gmail.com>
2021-08-05 21:51:03 +0900

    ✨feat:404페이지 수정, 필요없는 코드 삭제

--- src/components/MenuList/MenuList.js
+++ src/components/MenuList/MenuList.js
@@ -1,20 +1,7 @@
 import "./style.css";
-import { UserContainer } from "container/User";
 
 const MenuList = props => {
-  const { history } = props;
-  const { userInfo } = UserContainer.useContainer();
-
-  return (
-    <div className="menu-list">
-      <ul>
-        <li>홈</li>
-        {userInfo?.menus.map((item, index) => (
-          <li key={item.id + index}>{item.menu_name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div className="menu-list"></div>;
 };
 
 export default MenuList;
--- src/pages/page404.js
+++ src/pages/page404.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const page404 = () => {
-  return <div>페이지를 찾을 수 없습니다!!</div>;
+const page404 = props => {
+  const _handleOnClick = () => {
+    props.history.push({
+      pathname: `/`,
+    });
+  };
+
+  return (
+    <div className="error-wrap">
+      <h1>404</h1>
+      <div>찾을 수 없는 페이지입니다.</div>
+      <div>요청하신 페이지가 사라졌거나, 잘못된 경로를 이용하셨습니다 : )</div>
+      <button onClick={_handleOnClick}>홈으로</button>
+    </div>
+  );
 };
 
 export { page404 };

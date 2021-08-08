e5650399fdf754bd59b2b4483a0844be7333270f
HEEYOUNG SEO <hseo@HEEYOUNGs-iMac.local>
2021-08-09 02:01:00 +0900

    validate only onSubmit

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -14,19 +14,24 @@ const Register = props => {
     if (newUser.email && !VALID_EMAIL.test(newUser.email)) {
       setErrorMessage("유효한 메일 주소를 입력하세요");
       return 0;
-    } else if (newUser.password && !VALID_PASSWORD.test(newUser.password)) {
+    }
+    if (newUser.password && !VALID_PASSWORD.test(newUser.password)) {
       setErrorMessage("비밀번호는 영문 대소문자, 특수문자, 숫자를 포함하여 8자리 이상 입력해주세요");
       return 0;
-    } else if (newUser.password !== "" && newUser.re_password && newUser.re_password !== newUser.password) {
+    }
+    if (newUser.password !== "" && newUser.re_password && newUser.re_password !== newUser.password) {
       setErrorMessage("비밀번호가 일치하지 않습니다");
       return 0;
-    } else if (newUser.age < 0 || newUser.age > 100) {
+    }
+    if (newUser.age < 0 || newUser.age > 100) {
       setErrorMessage("0~100 사이로 나이를 입력하세요");
       return 0;
-    } else if (newUser.card_info && !VALID_CREDITCARD.test(newUser.card_info)) {
+    }
+    if (newUser.card_info && !VALID_CREDITCARD.test(newUser.card_info)) {
       setErrorMessage("유효한 카드번호를 입력하세요");
       return 0;
-    } else if (newUser.address === "") {
+    }
+    if (newUser.address === "") {
       setErrorMessage("주소를 입력하세요");
       return 0;
     }
@@ -62,7 +67,6 @@ const Register = props => {
   const handleChange = e => {
     const { name, value } = e.target;
     setNewUser({ ...newUser, [name]: value });
-    validateInput();
   };
 
   const handleAddress = value => {

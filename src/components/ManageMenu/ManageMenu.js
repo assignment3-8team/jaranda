533bd5357953e63af6f91550d5853fffb1b749d0
HEEYOUNG SEO <hseo@HEEYOUNGs-iMac.local>
2021-08-12 16:20:02 +0900

    :hammer: fix: menageMenu에 form tag 이용

--- src/components/ManageMenu/ManageMenu.js
+++ src/components/ManageMenu/ManageMenu.js
@@ -59,46 +59,48 @@ const ManageMenu = props => {
   return (
     <>
       <div className="wrapper">
-        {id ? (
-          <UserDetails data={props.userData} />
-        ) : (
-          <SignUp onChange={onChange} handleAddress={handleAddress} data={data} errors={errors} isAdminMenu={true} />
-        )}
-        <div className="select-box">
-          <div className="not-allowed-zone">
-            <p>허용하지 않는 메뉴</p>
-            {allowedMenuList.map(item => {
-              return (
-                !item.checked && (
-                  <label key={item.menu_name}>
-                    <span>{MENU_NAME[item.menu_name]}</span>
-                    <input type="checkbox" id={item.id} onChange={e => onCheckMenu(e)} name={item.menu_name} value={item.menu_name} />
-                  </label>
-                )
-              );
-            })}
+        <form onSubmit={handleSubmit}>
+          {id ? (
+            <UserDetails data={props.userData} />
+          ) : (
+            <SignUp onChange={onChange} handleAddress={handleAddress} data={data} errors={errors} isAdminMenu={true} />
+          )}
+          <div className="select-box">
+            <div className="not-allowed-zone">
+              <p>허용하지 않는 메뉴</p>
+              {allowedMenuList.map(item => {
+                return (
+                  !item.checked && (
+                    <label key={item.menu_name}>
+                      <span>{MENU_NAME[item.menu_name]}</span>
+                      <input type="checkbox" id={item.id} onChange={e => onCheckMenu(e)} name={item.menu_name} value={item.menu_name} />
+                    </label>
+                  )
+                );
+              })}
+            </div>
+            <div className="arrow-wrap">
+              <div>▶️</div>
+              <div>◀️</div>
+            </div>
+            <div className="allowed-zone">
+              <p>허용하는 메뉴</p>
+              {allowedMenuList.map((item, index) => {
+                return (
+                  item.checked && (
+                    <label key={item.id + index}>
+                      <span>{MENU_NAME[item.menu_name]}</span>
+                      <input type="checkbox" id={item.id} onChange={e => onCheckMenu(e)} name={item.menu_name} value={item.menu_name} />
+                    </label>
+                  )
+                );
+              })}
+            </div>
           </div>
-          <div className="arrow-wrap">
-            <div>▶️</div>
-            <div>◀️</div>
-          </div>
-          <div className="allowed-zone">
-            <p>허용하는 메뉴</p>
-            {allowedMenuList.map((item, index) => {
-              return (
-                item.checked && (
-                  <label key={item.id + index}>
-                    <span>{MENU_NAME[item.menu_name]}</span>
-                    <input type="checkbox" id={item.id} onChange={e => onCheckMenu(e)} name={item.menu_name} value={item.menu_name} />
-                  </label>
-                )
-              );
-            })}
-          </div>
-        </div>
-        <button className="save-button" onClick={handleSubmit}>
-          저장
-        </button>
+          <button className="save-button" type="submit">
+            저장
+          </button>
+        </form>
       </div>
     </>
   );
